92c15d87ec1c4900bef54ea14d4ae55aa3c1da65
hannahsummerfield <71515508+hannahsummerfield@users.noreply.github.com>
2020-09-18 11:12:06 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,21 @@
+// When the user shakes the micro:bit, a frown face appears.
+input.onGesture(Gesture.Shake, function () {
+	
+})
+// When the micro:bit isn't shaking, a smiley face appears.
+basic.forever(function () {
+    basic.showLeds(`
+        . # . # .
+        . # . # .
+        . . . . .
+        . # # # .
+        # . . . #
+        `)
+    basic.showLeds(`
+        . # . # .
+        . # . # .
+        . . . . .
+        # . . . #
+        . # # # .
+        `)
+})
